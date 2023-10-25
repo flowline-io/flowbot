91f7b33f0dc0c3c2b95aa52b14b2adc14ee7bfb1
sysatom <sysatom@gmail.com>
2023-10-25 21:12:16 +0800

    shadcn-ui

--- internal/bots/dev/webapp/vite.config.ts
+++ internal/bots/dev/webapp/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -9,6 +10,11 @@ export default defineConfig(({ command, mode }) => {
     plugins: [react()],
     build: {
       outDir: 'build',
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
     base: env.PUBLIC_URL,
   }
