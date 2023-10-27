27eb9dc15dc1902b47d84f2f90813f794edc550e
sysatom <sysatom@gmail.com>
2023-10-27 18:27:31 +0800

    api client

---
+++ internal/bots/web/webapp/src/client/schemas/$model_ReviewEvaluation.ts
@@ -0,0 +1,35 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export const $model_ReviewEvaluation = {
+  properties: {
+    created_at: {
+  type: 'string',
+},
+    id: {
+  type: 'number',
+},
+    question: {
+  type: 'string',
+},
+    reason: {
+  type: 'string',
+},
+    review_id: {
+  type: 'number',
+},
+    solving: {
+  type: 'string',
+},
+    topic: {
+  type: 'string',
+},
+    uid: {
+  type: 'string',
+},
+    updated_at: {
+  type: 'string',
+},
+  },
+} as const;
