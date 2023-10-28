a55cdf5885b8f76ca9d1eec438b492bb798532bc
sysatom <sysatom@gmail.com>
2023-10-28 16:44:57 +0800

    objective form page

--- internal/bots/web/webapp/src/main.tsx
+++ internal/bots/web/webapp/src/main.tsx
@@ -5,6 +5,7 @@ import {RouterProvider} from "react-router-dom";
 import router from "@/router.tsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {Toaster} from "@/components/ui/toaster";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
+      <Toaster />
     </QueryClientProvider>
   </React.StrictMode>,
 )
--- internal/bots/web/webapp/src/pages/objective-form.tsx
+++ internal/bots/web/webapp/src/pages/objective-form.tsx
@@ -9,21 +9,65 @@ import {TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
 import {Tabs} from "@radix-ui/react-tabs";
 import {Switch} from "@/components/ui/switch";
 import {cn} from "@/lib/utils";
-import { format } from "date-fns"
+import {format} from "date-fns"
 import {Popover, PopoverTrigger} from "@radix-ui/react-popover";
 import {PopoverContent} from "@/components/ui/popover";
 import {Calendar} from "@/components/ui/calendar";
 import {CalendarIcon} from "lucide-react";
 import React from "react";
 import {PersonIcon} from "@radix-ui/react-icons";
-import {Progress} from "@/components/ui/progress";
 import {Dialog, DialogTrigger} from "@radix-ui/react-dialog";
-import {DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle} from "@/components/ui/dialog";
+import {DialogContent, DialogFooter, DialogHeader, DialogTitle} from "@/components/ui/dialog";
 import {SelectGroup} from "@radix-ui/react-select";
+import * as z from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import {Client} from "@/util/client";
+import {useToast} from "@/components/ui/use-toast";
+
+const formSchema = z.object({
+  title: z.string().min(1).max(50),
+  is_plan: z.boolean(),
+  plan_start: z.date(),
+  plan_end: z.date(),
+  memo: z.string(),
+})
 
 export default function ObjectiveFormPage() {
-  const [dateStart, setDateStart] = React.useState<Date>()
-  const [dateEnd, setDateEnd] = React.useState<Date>()
+  const {toast} = useToast()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      is_plan: false,
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+    Client().okr.postOkrObjective(values).then((data) => {
+      console.log(data)
+      if (data.status == "ok") {
+        console.log("ok", data)
+      } else {
+        console.log("toast", data)
+        toast({
+          title: data.status,
+          description: data.message,
+          variant: "destructive",
+        })
+      }
+    }).catch(console.error)
+  }
 
   return (
     <>
@@ -41,87 +85,162 @@ export default function ObjectiveFormPage() {
                 <Tabs defaultValue="base" className="w-[600px]">
                   <TabsList>
                     <TabsTrigger value="base">基本信息</TabsTrigger>
-                    <TabsTrigger value="key-result">关键结果</TabsTrigger>
-                    <TabsTrigger value="motive">动机 & 可行性</TabsTrigger>
+                    <TabsTrigger value="key-result" className="hidden">关键结果</TabsTrigger>
+                    <TabsTrigger value="motive" className="hidden">动机 & 可行性</TabsTrigger>
                   </TabsList>
                   <TabsContent value="base">
-                    <div className="grid gap-6">
-                      <div className="grid gap-2">
-                        <Label htmlFor="subject">目标标题</Label>
-                        <Input id="subject" placeholder="I need help with..." />
-                      </div>
-                      <div className="grid gap-2">
-                        <Label htmlFor="subject">计划</Label>
-                        <div className="grid grid-cols-3 gap-4">
+                    <Form {...form}>
+                      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <div className="grid gap-6">
                           <div className="grid gap-2">
-                            <Label htmlFor="area">开启</Label>
-                            <Switch />
+                            <FormField
+                              control={form.control}
+                              name="title"
+                              render={({field}) => (
+                                <FormItem>
+                                  <FormLabel>标题</FormLabel>
+                                  <FormControl>
+                                    <Input placeholder="shadcn" {...field} />
+                                  </FormControl>
+                                  <FormMessage/>
+                                </FormItem>
+                              )}
+                            />
                           </div>
                           <div className="grid gap-2">
-                            <Label htmlFor="area">开始日期</Label>
-                            <Popover>
-                              <PopoverTrigger asChild>
-                                <Button
-                                  variant={"outline"}
-                                  className={cn(
-                                    "w-[180px] justify-start text-left font-normal",
-                                    !dateStart && "text-muted-foreground"
+                            <Label htmlFor="subject">计划</Label>
+                            <div className="grid grid-cols-3 gap-4">
+                              <div className="grid gap-2">
+                                <FormField
+                                  control={form.control}
+                                  name="is_plan"
+                                  render={({field}) => (
+                                    <FormItem>
+                                      <FormLabel>开启</FormLabel>
+                                      <FormControl>
+                                        <Switch checked={field.value} onCheckedChange={field.onChange}/>
+                                      </FormControl>
+                                      <FormMessage/>
+                                    </FormItem>
                                   )}
-                                >
-                                  <CalendarIcon className="mr-2 h-4 w-4" />
-                                  {dateStart ? format(dateStart, "PPP") : <span>Pick a date</span>}
-                                </Button>
-                              </PopoverTrigger>
-                              <PopoverContent className="w-auto p-0">
-                                <Calendar
-                                  mode="single"
-                                  selected={dateStart}
-                                  onSelect={setDateStart}
-                                  initialFocus
                                 />
-                              </PopoverContent>
-                            </Popover>
+                              </div>
+                              <div className="grid gap-2">
+                                <FormField
+                                  control={form.control}
+                                  name="plan_start"
+                                  render={({field}) => (
+                                    <FormItem className="flex flex-col">
+                                      <FormLabel>开始日期</FormLabel>
+                                      <Popover>
+                                        <PopoverTrigger asChild>
+                                          <FormControl>
+                                            <Button
+                                              variant={"outline"}
+                                              className={cn(
+                                                "w-[180px] pl-3 text-left font-normal",
+                                                !field.value && "text-muted-foreground"
+                                              )}
+                                            >
+                                              {field.value ? (
+                                                format(field.value, "PPP")
+                                              ) : (
+                                                <span>Pick a date</span>
+                                              )}
+                                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50"/>
+                                            </Button>
+                                          </FormControl>
+                                        </PopoverTrigger>
+                                        <PopoverContent className="w-auto p-0" align="start">
+                                          <Calendar
+                                            mode="single"
+                                            selected={field.value}
+                                            onSelect={field.onChange}
+                                            disabled={(date) =>
+                                              date > new Date() || date < new Date("1900-01-01")
+                                            }
+                                            initialFocus
+                                          />
+                                        </PopoverContent>
+                                      </Popover>
+                                      <FormMessage/>
+                                    </FormItem>
+                                  )}
+                                />
+                              </div>
+                              <div className="grid gap-2">
+                                <FormField
+                                  control={form.control}
+                                  name="plan_end"
+                                  render={({field}) => (
+                                    <FormItem className="flex flex-col">
+                                      <FormLabel>结束日期</FormLabel>
+                                      <Popover>
+                                        <PopoverTrigger asChild>
+                                          <FormControl>
+                                            <Button
+                                              variant={"outline"}
+                                              className={cn(
+                                                "w-[180px] pl-3 text-left font-normal",
+                                                !field.value && "text-muted-foreground"
+                                              )}
+                                            >
+                                              {field.value ? (
+                                                format(field.value, "PPP")
+                                              ) : (
+                                                <span>Pick a date</span>
+                                              )}
+                                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50"/>
+                                            </Button>
+                                          </FormControl>
+                                        </PopoverTrigger>
+                                        <PopoverContent className="w-auto p-0" align="start">
+                                          <Calendar
+                                            mode="single"
+                                            selected={field.value}
+                                            onSelect={field.onChange}
+                                            disabled={(date) =>
+                                              date > new Date() || date < new Date("1900-01-01")
+                                            }
+                                            initialFocus
+                                          />
+                                        </PopoverContent>
+                                      </Popover>
+                                      <FormMessage/>
+                                    </FormItem>
+                                  )}
+                                />
+                              </div>
+                            </div>
                           </div>
                           <div className="grid gap-2">
-                            <Label htmlFor="security-level">结束日期</Label>
-                            <Popover>
-                              <PopoverTrigger asChild>
-                                <Button
-                                  variant={"outline"}
-                                  className={cn(
-                                    "w-[180px] justify-start text-left font-normal",
-                                    !dateEnd && "text-muted-foreground"
-                                  )}
-                                >
-                                  <CalendarIcon className="mr-2 h-4 w-4" />
-                                  {dateEnd ? format(dateEnd, "PPP") : <span>Pick a date</span>}
-                                </Button>
-                              </PopoverTrigger>
-                              <PopoverContent className="w-auto p-0">
-                                <Calendar
-                                  mode="single"
-                                  selected={dateEnd}
-                                  onSelect={setDateEnd}
-                                  initialFocus
-                                />
-                              </PopoverContent>
-                            </Popover>
+                            <FormField
+                              control={form.control}
+                              name="memo"
+                              render={({field}) => (
+                                <FormItem>
+                                  <FormLabel>备忘</FormLabel>
+                                  <FormControl>
+                                    <Textarea placeholder="shadcn" {...field} />
+                                  </FormControl>
+                                  <FormMessage/>
+                                </FormItem>
+                              )}
+                            />
                           </div>
                         </div>
-                      </div>
-                      <div className="grid gap-2">
-                        <Label htmlFor="description">备忘</Label>
-                        <Textarea
-                          id="description"
-                          placeholder="Please include all information relevant to your issue."
-                        />
-                      </div>
-                    </div>
+                        <div className="grid gap-6">
+                          <Button type="submit">Submit</Button>
+                          <Button variant="ghost">Cancel</Button>
+                        </div>
+                      </form>
+                    </Form>
                   </TabsContent>
                   <TabsContent value="key-result">
                     <div className="grid gap-6">
                       <div className="grid grid-cols-1 gap-4">
-                        <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
+                        <div
+                          className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
                           <PersonIcon className="mt-px h-5 w-5"/>
                           <div className="space-y-1">
                             <div className="text-sm font-medium leading-none">关键结果 1</div>
@@ -182,7 +301,7 @@ export default function ObjectiveFormPage() {
                               </Label>
                               <Select>
                                 <SelectTrigger className="w-[180px]">
-                                  <SelectValue placeholder="Select a fruit" />
+                                  <SelectValue placeholder="Select a fruit"/>
                                 </SelectTrigger>
                                 <SelectContent>
                                   <SelectGroup>
@@ -227,7 +346,8 @@ export default function ObjectiveFormPage() {
                   <TabsContent value="motive">
                     <div className="grid gap-6">
                       <div className="grid grid-cols-1 gap-4">
-                        <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
+                        <div
+                          className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
                           <PersonIcon className="mt-px h-5 w-5"/>
                           <div className="space-y-1">
                             <div className="text-sm font-medium leading-none">动机 1</div>
@@ -254,7 +374,7 @@ export default function ObjectiveFormPage() {
                               </Label>
                               <Select>
                                 <SelectTrigger className="w-[180px]">
-                                  <SelectValue placeholder="Select a fruit" />
+                                  <SelectValue placeholder="Select a fruit"/>
                                 </SelectTrigger>
                                 <SelectContent>
                                   <SelectGroup>
@@ -292,10 +412,6 @@ export default function ObjectiveFormPage() {
                   </TabsContent>
                 </Tabs>
               </CardContent>
-              <CardFooter className="justify-between space-x-2">
-                <Button variant="ghost">Cancel</Button>
-                <Button>Submit</Button>
-              </CardFooter>
             </Card>
           </FlexContainer>
         </div>
