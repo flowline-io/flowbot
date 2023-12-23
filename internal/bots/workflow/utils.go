659586657c914522afe26f88647e7d653fe67442
sysatom <sysatom@gmail.com>
2023-12-23 23:02:41 +0800

    workflow script parse

--- internal/bots/workflow/utils.go
+++ internal/bots/workflow/utils.go
@@ -2,17 +2,29 @@ package workflow
 
 import (
 	"errors"
+	"fmt"
 	"github.com/flowline-io/flowbot/internal/store/model"
 	"github.com/flowline-io/flowbot/internal/types"
 	"github.com/flowline-io/flowbot/pkg/utils"
 	"github.com/robfig/cron/v3"
 	"gopkg.in/yaml.v3"
+	"regexp"
 	"strings"
 )
 
-func ParseYamlWorkflow(code string) (workflow *model.Workflow, trigger *model.WorkflowTrigger, dag *model.Dag, err error) {
+const arrowSymbol = "->"
+
+func parseWorkflowMetadata(code string) (types.WorkflowMetadata, error) {
 	var meta types.WorkflowMetadata
-	err = yaml.Unmarshal(utils.StringToBytes(code), &meta)
+	err := yaml.Unmarshal(utils.StringToBytes(code), &meta)
+	if err != nil {
+		return types.WorkflowMetadata{}, err
+	}
+	return meta, nil
+}
+
+func ParseYamlWorkflow(code string) (workflow *model.Workflow, trigger *model.WorkflowTrigger, dag *model.Dag, err error) {
+	meta, err := parseWorkflowMetadata(code)
 	if err != nil {
 		return
 	}
@@ -34,6 +46,43 @@ func ParseYamlWorkflow(code string) (workflow *model.Workflow, trigger *model.Wo
 
 	// dag
 	dag = new(model.Dag)
+
+	nodes := make([]*model.Node, 0, len(meta.Tasks))
+	for _, task := range meta.Tasks {
+		botName, ruleId, ok := parseTaskAction(task.Action)
+		if !ok {
+			err = fmt.Errorf("task action %s parse error", task.Action)
+			return
+		}
+		nodes = append(nodes, &model.Node{
+			Id:          task.ID,
+			Describe:    task.Describe,
+			Bot:         botName,
+			RuleId:      ruleId,
+			Parameters:  task.Params,
+			Variables:   task.Vars,
+			Connections: task.Conn,
+			Status:      model.NodeDefault,
+		})
+	}
+
+	edges := make([]*model.Edge, 0, len(meta.Tasks))
+
+	index := 1
+	for _, pipeline := range meta.Pipeline {
+		pipelineEdges := parsePipelineEdges(pipeline)
+		for _, edge := range pipelineEdges {
+			edges = append(edges, &model.Edge{
+				Id:     fmt.Sprintf("edge-%d", index),
+				Source: edge[0],
+				Target: edge[1],
+			})
+			index += 1
+		}
+	}
+
+	dag.Nodes = nodes
+	dag.Edges = edges
 
 	return
 }
@@ -71,7 +120,7 @@ func MetaDataValidate(meta types.WorkflowMetadata) (err error) {
 			return
 		}
 	case model.TriggerWebhook:
-		// todo webhook
+		// todo webhook params validate
 	}
 
 	if len(meta.Pipeline) == 0 {
@@ -79,10 +128,135 @@ func MetaDataValidate(meta types.WorkflowMetadata) (err error) {
 		return
 	}
 
+	for index, pipeline := range meta.Pipeline {
+		if !pipelineFormatValidate(pipeline) {
+			err = fmt.Errorf("line %d pipeline %s format error", index+1, pipeline)
+			return
+		}
+	}
+
 	if len(meta.Tasks) == 0 {
 		err = errors.New("tasks empty")
 		return
 	}
 
+	re, err := regexp.Compile(`^[a-z_]+@[a-z_]+$`)
+	if err != nil {
+		return
+	}
+
+	taskIdSet := make(map[string]struct{})
+	for _, task := range meta.Tasks {
+		if strings.TrimSpace(task.ID) == "" {
+			err = errors.New("task id empty")
+			return
+		}
+		if _, ok := taskIdSet[task.ID]; ok {
+			err = fmt.Errorf("task id %s duplicated", task.ID)
+			return
+		}
+		taskIdSet[task.ID] = struct{}{}
+
+		if strings.TrimSpace(task.Action) == "" {
+			err = errors.New("task action empty")
+			return
+		}
+		if !re.Match(utils.StringToBytes(task.Action)) {
+			err = errors.New("task action format error, e.g. action@bot")
+			return
+		}
+	}
+
+	spaceRe, err := regexp.Compile(`\s+`)
+	if err != nil {
+		return
+	}
+	for _, pipeline := range meta.Pipeline {
+		tokens := spaceRe.Split(pipeline, -1)
+		for _, token := range tokens {
+			if token == arrowSymbol {
+				continue
+			}
+			if _, ok := taskIdSet[token]; !ok {
+				err = fmt.Errorf("pipeline node id %s not exist in tasks", token)
+				return
+			}
+		}
+	}
+
 	return
+}
+
+func pipelineFormatValidate(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+
+	s = strings.TrimSpace(s)
+
+	re, err := regexp.Compile(`\s+`)
+	if err != nil {
+		return false
+	}
+
+	tokens := re.Split(s, -1)
+	l := len(tokens)
+
+	if len(tokens) == 1 || len(tokens) == 2 {
+		return false
+	}
+
+	if tokens[l-1] == arrowSymbol {
+		return false
+	}
+
+	i := 2
+	for i < l {
+		if !(tokens[i-2] != arrowSymbol && tokens[i-1] == arrowSymbol && tokens[i] != arrowSymbol) {
+			return false
+		}
+		i += 2
+	}
+
+	return true
+}
+
+func parseTaskAction(action string) (string, string, bool) {
+	tokens := strings.Split(action, "@")
+
+	if len(tokens) != 2 {
+		return "", "", false
+	}
+
+	return tokens[0], tokens[1], true
+}
+
+func parsePipelineEdges(pipeline string) [][2]string {
+	if pipeline == "" {
+		return nil
+	}
+
+	pipeline = strings.TrimSpace(pipeline)
+
+	re, err := regexp.Compile(`\s+`)
+	if err != nil {
+		return nil
+	}
+
+	tokens := re.Split(pipeline, -1)
+	l := len(tokens)
+
+	if l < 3 {
+		return nil
+	}
+
+	i := 2
+
+	edges := make([][2]string, 0)
+	for i < l {
+		edges = append(edges, [2]string{tokens[i-2], tokens[i]})
+		i += 2
+	}
+
+	return edges
 }
--- internal/bots/workflow/utils_test.go
+++ internal/bots/workflow/utils_test.go
@@ -2,26 +2,12 @@ package workflow
 
 import (
 	"github.com/flowline-io/flowbot/internal/store/model"
+	"github.com/flowline-io/flowbot/internal/types"
 	"reflect"
 	"testing"
 )
 
-func TestParseYamlWorkflow(t *testing.T) {
-	type args struct {
-		code string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *model.Workflow
-		want1   *model.WorkflowTrigger
-		want2   *model.Dag
-		wantErr bool
-	}{
-		{
-			name: "ok",
-			args: args{
-				code: `---
+const okYaml = `---
 name: example
 describe: do something...
 
@@ -57,7 +43,53 @@ tasks:
     action: out_workflow_action@dev
 
   - id: out2
-    action: out_workflow_action@dev`,
+    action: out_workflow_action@dev`
+
+const failYaml = `---
+name: example
+describe: do something...
+
+trigger:
+  type: cron # cron, manual, webhook
+  rule:
+    spec: '* * *' # if cron
+
+
+pipeline:
+  - input -> add_two_number -> out1
+  - add_two_number -> out2
+
+
+tasks:
+  - id: input
+    action: in_workflow_action
+    describe: do something... # optional
+    params: # optional
+      param1: val1
+      param2: val2
+    vars: # optional
+      - var1
+      - var2
+    conn: # optional
+      - conn1
+      - conn2`
+
+func TestParseYamlWorkflow(t *testing.T) {
+	type args struct {
+		code string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *model.Workflow
+		want1   *model.WorkflowTrigger
+		want2   *model.Dag
+		wantErr bool
+	}{
+		{
+			name: "ok",
+			args: args{
+				code: okYaml,
 			},
 			want: &model.Workflow{
 				Name:     "example",
@@ -120,6 +152,16 @@ tasks:
 			},
 			wantErr: false,
 		},
+		{
+			name: "fail",
+			args: args{
+				code: failYaml,
+			},
+			want:    nil,
+			want1:   nil,
+			want2:   nil,
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -136,6 +178,187 @@ tasks:
 			}
 			if !reflect.DeepEqual(got2, tt.want2) {
 				t.Errorf("ParseYamlWorkflow() got2 = %v, want %v", got2, tt.want2)
+			}
+		})
+	}
+}
+
+func TestMetaDataValidate(t *testing.T) {
+	okMeta, err := parseWorkflowMetadata(okYaml)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	failMeta, err := parseWorkflowMetadata(failYaml)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	type args struct {
+		meta types.WorkflowMetadata
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "ok",
+			args: args{
+				meta: okMeta,
+			},
+			wantErr: false,
+		},
+		{
+			name: "fail",
+			args: args{
+				meta: failMeta,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := MetaDataValidate(tt.args.meta); (err != nil) != tt.wantErr {
+				t.Errorf("MetaDataValidate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_pipelineFormatValidate(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "six nodes",
+			args: args{
+				s: "input -> add_two_number -> out1 -> debug -> archive -> end",
+			},
+			want: true,
+		},
+		{
+			name: "five nodes",
+			args: args{
+				s: "input -> add_two_number -> out1 -> debug -> archive",
+			},
+			want: true,
+		},
+		{
+			name: "four nodes",
+			args: args{
+				s: "input -> add_two_number -> out1 -> debug",
+			},
+			want: true,
+		},
+		{
+			name: "three nodes",
+			args: args{
+				s: "input -> add_two_number -> out1",
+			},
+			want: true,
+		},
+		{
+			name: "two nodes",
+			args: args{
+				s: "add_two_number -> out1",
+			},
+			want: true,
+		},
+		{
+			name: "one node",
+			args: args{
+				s: "add_two_number",
+			},
+			want: false,
+		},
+		{
+			name: "spaces",
+			args: args{
+				s: "   add_two_number  ->    out1 ",
+			},
+			want: true,
+		},
+		{
+			name: "error first",
+			args: args{
+				s: "-> add_two_number -> out1",
+			},
+			want: false,
+		},
+		{
+			name: "error last",
+			args: args{
+				s: "add_two_number -> out1  ->",
+			},
+			want: false,
+		},
+		{
+			name: "empty node",
+			args: args{
+				s: "->",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := pipelineFormatValidate(tt.args.s); got != tt.want {
+				t.Errorf("pipelineFormatValidate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parsePipelineEdges(t *testing.T) {
+	type args struct {
+		pipeline string
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][2]string
+	}{
+		{
+			name: "case1",
+			args: args{
+				pipeline: "a -> b -> c -> d",
+			},
+			want: [][2]string{
+				{"a", "b"},
+				{"b", "c"},
+				{"c", "d"},
+			},
+		},
+		{
+			name: "case2",
+			args: args{
+				pipeline: "a -> b -> c",
+			},
+			want: [][2]string{
+				{"a", "b"},
+				{"b", "c"},
+			},
+		},
+		{
+			name: "case3",
+			args: args{
+				pipeline: "a -> b",
+			},
+			want: [][2]string{
+				{"a", "b"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parsePipelineEdges(tt.args.pipeline); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parsePipelineEdges() = %v, want %v", got, tt.want)
 			}
 		})
 	}
