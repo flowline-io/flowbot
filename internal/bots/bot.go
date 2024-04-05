1ff7e2bf4cd53838a4b8192276a61b6962883832
sysatom <sysatom@gmail.com>
2024-04-06 00:23:12 +0800

    GitMoji: 🚀  + internal/bots: Add interface definition and base implementation
    ℹ️ Define the Handler interface with methods for bot initialization, event handling, help, rules, input, command, form, action, session, cron, condition, group, pipeline, agent, instruct, page, webservice, webapp, workflow, and webhook in internal/bots/interface.go. Add a Base struct with default implementations for these methods. Update internal/bots/bot.go to remove duplicate interface and base implementations. Register handlers and initialize them with Init method.

--- internal/bots/bot.go
+++ internal/bots/bot.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/emicklei/go-restful/v3"
 	"github.com/flowline-io/flowbot/internal/ruleset/action"
 	"github.com/flowline-io/flowbot/internal/ruleset/agent"
 	"github.com/flowline-io/flowbot/internal/ruleset/command"
@@ -37,163 +36,6 @@ import (
 	"strings"
 	"time"
 )
-
-type Handler interface {
-	// Init initializes the bot.
-	Init(jsonconf json.RawMessage) error
-
-	// IsReady сhecks if the bot is initialized.
-	IsReady() bool
-
-	// Bootstrap Lifecycle hook
-	Bootstrap() error
-
-	// OnEvent event
-	OnEvent() error
-
-	// Help return bot help
-	Help() (map[string][]string, error)
-
-	// Rules return bot ruleset
-	Rules() []interface{}
-
-	// Input return input result
-	Input(ctx types.Context, head types.KV, content interface{}) (types.MsgPayload, error)
-
-	// Command return bot result
-	Command(ctx types.Context, content interface{}) (types.MsgPayload, error)
-
-	// Form return bot form result
-	Form(ctx types.Context, values types.KV) (types.MsgPayload, error)
-
-	// Action return bot action result
-	Action(ctx types.Context, option string) (types.MsgPayload, error)
-
-	// Session return bot session result
-	Session(ctx types.Context, content interface{}) (types.MsgPayload, error)
-
-	// Cron cron script daemon
-	Cron(send types.SendFunc) (*cron.Ruleset, error)
-
-	// Condition run conditional process
-	Condition(ctx types.Context, forwarded types.MsgPayload) (types.MsgPayload, error)
-
-	// Group return group result
-	Group(ctx types.Context, head types.KV, content interface{}) (types.MsgPayload, error)
-
-	// Pipeline return pipeline result
-	Pipeline(ctx types.Context, head types.KV, content interface{}, operate types.PipelineOperate) (types.MsgPayload, string, int, error)
-
-	// Agent return group result
-	Agent(ctx types.Context, content types.KV) (types.MsgPayload, error)
-
-	// Instruct return instruct list
-	Instruct() (instruct.Ruleset, error)
-
-	// Page return page
-	Page(ctx types.Context, flag string) (string, error)
-
-	// Webservice return webservice routes
-	Webservice(app *fiber.App)
-
-	// Webapp return webapp
-	Webapp() func(rw http.ResponseWriter, req *http.Request)
-
-	// Workflow return workflow result
-	Workflow(ctx types.Context, input types.KV) (types.KV, error)
-
-	// Webhook return webhook result
-	Webhook(ctx types.Context, content types.KV) (types.MsgPayload, error)
-}
-
-type Base struct{}
-
-func (Base) Bootstrap() error {
-	return nil
-}
-
-func (Base) WebService() *restful.WebService {
-	return nil
-}
-
-func (Base) OnEvent() error {
-	return nil
-}
-
-func (b Base) Help() (map[string][]string, error) {
-	return Help(b.Rules())
-}
-
-func (Base) Rules() []interface{} {
-	return nil
-}
-
-func (Base) Input(_ types.Context, _ types.KV, _ interface{}) (types.MsgPayload, error) {
-	return nil, nil
-}
-
-func (Base) Command(_ types.Context, _ interface{}) (types.MsgPayload, error) {
-	return nil, nil
-}
-
-func (Base) Form(_ types.Context, _ types.KV) (types.MsgPayload, error) {
-	return nil, nil
-}
-
-func (Base) Action(_ types.Context, _ string) (types.MsgPayload, error) {
-	return nil, nil
-}
-
-func (Base) Session(_ types.Context, _ interface{}) (types.MsgPayload, error) {
-	return nil, nil
-}
-
-func (Base) Cron(_ types.SendFunc) (*cron.Ruleset, error) {
-	return nil, nil
-}
-
-func (Base) Condition(_ types.Context, _ types.MsgPayload) (types.MsgPayload, error) {
-	return nil, nil
-}
-
-func (Base) Group(_ types.Context, _ types.KV, _ interface{}) (types.MsgPayload, error) {
-	return nil, nil
-}
-
-func (Base) Pipeline(_ types.Context, _ types.KV, _ interface{}, _ types.PipelineOperate) (types.MsgPayload, string, int, error) {
-	return nil, "", 0, nil
-}
-
-func (Base) Agent(_ types.Context, _ types.KV) (types.MsgPayload, error) {
-	return nil, nil
-}
-
-func (Base) Instruct() (instruct.Ruleset, error) {
-	return nil, nil
-}
-
-func (Base) Page(_ types.Context, _ string) (string, error) {
-	return "", nil
-}
-
-func (Base) Webservice(_ *fiber.App) {
-}
-
-func (Base) Webapp() func(rw http.ResponseWriter, req *http.Request) {
-	return nil
-}
-
-func (Base) Workflow(_ types.Context, _ types.KV) (types.KV, error) {
-	return nil, nil
-}
-
-func (Base) Webhook(_ types.Context, _ types.KV) (types.MsgPayload, error) {
-	return nil, nil
-}
-
-type configType struct {
-	Name string `json:"name"`
-}
 
 var handlers map[string]Handler
 
@@ -1067,6 +909,10 @@ func Webservice(app *fiber.App, name string, ruleset webservice.Ruleset) {
 		routes = append(routes, route.Route(rule.Method, rule.Path, rule.Function, rule.Option...))
 	}
 	route.WebService(app, name, routes...)
+}
+
+type configType struct {
+	Name string `json:"name"`
 }
 
 // Init initializes registered handlers.
