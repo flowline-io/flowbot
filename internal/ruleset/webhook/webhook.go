1551fc4e0c4ccefa061fc5eb1b87d46ff1396649
sysatom <sysatom@gmail.com>
2024-04-06 00:08:19 +0800

    :sparkles::heavy_plus_sign: Add new webhook processing ruleset in webhook.go for Flowbot project.

---
+++ internal/ruleset/webhook/webhook.go
@@ -0,0 +1,21 @@
+package webhook
+
+import "github.com/flowline-io/flowbot/internal/types"
+
+type Rule struct {
+	Id      string
+	Secret  bool
+	Handler func(ctx types.Context, content types.KV) types.MsgPayload
+}
+
+type Ruleset []Rule
+
+func (r Ruleset) Process(ctx types.Context, content types.KV) (types.MsgPayload, error) {
+	for _, rule := range r {
+		result := rule.Handler(ctx, content)
+		if result != nil {
+			return result, nil
+		}
+	}
+	return nil, nil
+}
