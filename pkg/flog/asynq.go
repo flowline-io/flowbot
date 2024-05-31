33322a977b8b5fa2822995fee2471cf657ef111b
sysatom <sysatom@gmail.com>
2024-05-31 09:46:57 +0000

    fix: asynq log caller

--- pkg/flog/asynq.go
+++ pkg/flog/asynq.go
@@ -13,23 +13,23 @@ type asynqLogger struct {
 }
 
 func (a *asynqLogger) Debug(args ...interface{}) {
-	l.Debug().Caller(3).Msg(fmt.Sprint(args...))
+	l.Debug().Caller(2).Msg(fmt.Sprint(args...))
 }
 
 func (a *asynqLogger) Info(args ...interface{}) {
-	l.Info().Caller(3).Msg(fmt.Sprint(args...))
+	l.Info().Caller(2).Msg(fmt.Sprint(args...))
 }
 
 func (a *asynqLogger) Warn(args ...interface{}) {
-	l.Warn().Caller(3).Msg(fmt.Sprint(args...))
+	l.Warn().Caller(2).Msg(fmt.Sprint(args...))
 }
 
 func (a *asynqLogger) Error(args ...interface{}) {
-	l.Error().Caller(3).Msg(fmt.Sprint(args...))
+	l.Error().Caller(2).Msg(fmt.Sprint(args...))
 }
 
 func (a *asynqLogger) Fatal(args ...interface{}) {
-	l.Fatal().Caller(3).Msg(fmt.Sprint(args...))
+	l.Fatal().Caller(2).Msg(fmt.Sprint(args...))
 }
 
 func AsynqLogLevel(level string) asynq.LogLevel {
