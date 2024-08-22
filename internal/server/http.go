7013588e091eb7aee187ec3562bc7f453791be26
sysatom <sysatom@gmail.com>
2024-08-22 09:05:36 +0000

    update serveStatus

--- internal/server/http.go
+++ internal/server/http.go
@@ -91,41 +91,11 @@ func signalHandler() <-chan bool {
 	return stop
 }
 
-// debugSession is session debug info.
-type debugSession struct {
-	RemoteAddr string   `json:"remote_addr,omitempty"`
-	Ua         string   `json:"ua,omitempty"`
-	Uid        string   `json:"uid,omitempty"`
-	Sid        string   `json:"sid,omitempty"`
-	Clnode     string   `json:"clnode,omitempty"`
-	Subs       []string `json:"subs,omitempty"`
-}
-
-// debugTopic is a topic debug info.
-type debugTopic struct {
-	Topic    string   `json:"topic,omitempty"`
-	Xorig    string   `json:"xorig,omitempty"`
-	IsProxy  bool     `json:"is_proxy,omitempty"`
-	PerUser  []string `json:"per_user,omitempty"`
-	PerSubs  []string `json:"per_subs,omitempty"`
-	Sessions []string `json:"sessions,omitempty"`
-}
-
-// debugCachedUser is a user cache entry debug info.
-type debugCachedUser struct {
-	Uid    string `json:"uid,omitempty"`
-	Unread int    `json:"unread,omitempty"`
-	Topics int    `json:"topics,omitempty"`
-}
-
 // debugDump is server internal state dump for debugging.
 type debugDump struct {
-	Version   string            `json:"server_version,omitempty"`
-	Build     string            `json:"build_id,omitempty"`
-	Timestamp time.Time         `json:"ts,omitempty"`
-	Sessions  []debugSession    `json:"sessions,omitempty"`
-	Topics    []debugTopic      `json:"topics,omitempty"`
-	UserCache []debugCachedUser `json:"user_cache,omitempty"`
+	Version   string    `json:"server_version,omitempty"`
+	Build     string    `json:"build_id,omitempty"`
+	Timestamp time.Time `json:"ts,omitempty"`
 }
 
 func serveStatus(wrt http.ResponseWriter, _ *http.Request) {
@@ -135,8 +105,6 @@ func serveStatus(wrt http.ResponseWriter, _ *http.Request) {
 		Version:   version.Buildtags,
 		Build:     version.Buildstamp,
 		Timestamp: types.TimeNow(),
-		Topics:    make([]debugTopic, 0, 10),
-		UserCache: make([]debugCachedUser, 0, 10),
 	}
 
 	_ = json.NewEncoder(wrt).Encode(result)
