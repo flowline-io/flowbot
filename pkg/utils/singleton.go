e79595c18524adfd22e227900e6f01fb296da78b
sysatom <sysatom@gmail.com>
2024-11-01 14:37:30 +0800

    update embed server

--- pkg/utils/singleton.go
+++ pkg/utils/singleton.go
@@ -2,14 +2,15 @@ package utils
 
 import (
 	"fmt"
+	"net"
+	"time"
+
 	"github.com/flowline-io/flowbot/pkg/flog"
 	"github.com/go-resty/resty/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
-	"net"
-	"time"
 )
 
 const EmbedServerPort = "5678"
@@ -17,7 +18,7 @@ const EmbedServerPort = "5678"
 func CheckSingleton() {
 	if !PortAvailable(EmbedServerPort) {
 		resp, err := resty.New().SetTimeout(500 * time.Millisecond).R().
-			Get(fmt.Sprintf("http://127.0.0.1:%s/", EmbedServerPort))
+			Get(fmt.Sprintf("http://127.0.0.1:%s/health", EmbedServerPort))
 		if err != nil {
 			flog.Error(err)
 			return
@@ -30,14 +31,15 @@ func CheckSingleton() {
 
 func EmbedServer() {
 	go func() {
-		app := fiber.New()
+		flog.Info("embed server http://127.0.0.1:%v", EmbedServerPort)
+
+		app := fiber.New(fiber.Config{DisableStartupMessage: true})
 		app.Use(cors.New())
 		app.Use(recover.New())
 		app.Use(requestid.New())
 
-		app.Get("/", func(c *fiber.Ctx) error {
-			return c.SendString("ok")
-		})
+		app.Get("/", func(c *fiber.Ctx) error { return nil })
+		app.Get("/health", func(c *fiber.Ctx) error { return c.SendString("ok") })
 
 		err := app.Listen(net.JoinHostPort("127.0.0.1", EmbedServerPort))
 		if err != nil {
