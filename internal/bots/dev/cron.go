50b09c2dd56aabf06957d9063ce8a5e8a1554803
sysatom <sysatom@gmail.com>
2024-11-07 09:54:25 +0800

    Docker images prune

--- internal/bots/dev/cron.go
+++ internal/bots/dev/cron.go
@@ -1,8 +1,13 @@
 package dev
 
 import (
+	"context"
+
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
 	"github.com/flowline-io/flowbot/internal/types"
 	"github.com/flowline-io/flowbot/internal/types/ruleset/cron"
+	"github.com/flowline-io/flowbot/pkg/flog"
 )
 
 var cronRules = []cron.Rule{
@@ -11,6 +16,26 @@ var cronRules = []cron.Rule{
 		Help: "cron example",
 		When: "0 */1 * * *",
 		Action: func(types.Context) []types.MsgPayload {
+			return nil
+		},
+	},
+	{
+		Name: "docker_images_prune",
+		Help: "Docker images prune",
+		When: "0 4 * * *",
+		Action: func(types.Context) []types.MsgPayload {
+			dc, err := client.NewClientWithOpts(client.FromEnv)
+			if err != nil {
+				flog.Error(err)
+				return nil
+			}
+			report, err := dc.ImagesPrune(context.Background(), filters.Args{})
+			if err != nil {
+				flog.Error(err)
+				return nil
+			}
+			flog.Info("docker prune report: %+v", report)
+
 			return nil
 		},
 	},
