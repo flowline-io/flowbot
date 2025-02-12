6341118bd16807a52bd16facd258a7269e7be58d
sysatom <sysatom@gmail.com>
2025-02-12 16:25:09 +0800

    opt get ai result

--- internal/bots/reader/utils.go
+++ internal/bots/reader/utils.go
@@ -1,8 +1,14 @@
 package reader
 
 import (
+	"context"
+	"errors"
 	"fmt"
 
+	"github.com/flowline-io/flowbot/pkg/providers"
+	openaiProvider "github.com/flowline-io/flowbot/pkg/providers/openai"
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/openai"
 	rssClient "miniflux.app/v2/client"
 )
 
@@ -13,10 +19,41 @@ func entriyFilter(entry *rssClient.Entry) bool {
 }
 
 func getAIResult(prompt, request string) (string, error) {
-	messages := []string{
+	tokenVal, _ := providers.GetConfig(openaiProvider.ID, openaiProvider.TokenKey)
+	baseUrlVal, _ := providers.GetConfig(openaiProvider.ID, openaiProvider.BaseUrlKey)
+	modelVal, _ := providers.GetConfig(openaiProvider.ID, openaiProvider.ModelKey)
+
+	llm, err := openai.New(
+		openai.WithToken(tokenVal.String()),
+		openai.WithBaseURL(baseUrlVal.String()),
+		openai.WithModel(modelVal.String()),
+	)
+	if err != nil {
+		return "", fmt.Errorf("openai new failed, %w", err)
+	}
+
+	messages := []llms.MessageContent{
 		// {"role": "system", "content": "You are a helpful assistant."},
 		// {"role": "user", "content": request + "\n---\n" + prompt},
+		{
+			Role:  llms.ChatMessageTypeSystem,
+			Parts: []llms.ContentPart{llms.TextContent{Text: "You are a helpful assistant."}},
+		},
+		{
+			Role:  llms.ChatMessageTypeHuman,
+			Parts: []llms.ContentPart{llms.TextContent{Text: request + "\n---\n" + prompt}},
+		},
 	}
-	_, _ = fmt.Println(messages)
-	return "", nil
+
+	resp, err := llm.GenerateContent(context.Background(), messages)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate content, %w", err)
+	}
+
+	choices := resp.Choices
+	if len(choices) < 1 {
+		return "", errors.New("empty response from model")
+	}
+	c1 := choices[0]
+	return c1.Content, nil
 }
