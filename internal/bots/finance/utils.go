087ecac2ea36a9757153280a175a6952041159dd
sysatom <sysatom@gmail.com>
2025-02-15 00:16:13 +0800

    ✨ feat(finance/utils): update classify function to use context and new prompt template

--- internal/bots/finance/utils.go
+++ internal/bots/finance/utils.go
@@ -1,17 +1,17 @@
 package finance
 
 import (
+	"context"
 	"fmt"
-
-	"github.com/tmc/langchaingo/prompts"
+	"github.com/cloudwego/eino/components/prompt"
+	"github.com/cloudwego/eino/schema"
 )
 
-func classify() {
-	prompt := prompts.NewPromptTemplate(
-		`Given i want to categorize transactions on my bank account into this categories: {{.categories}}
+func classify(ctx context.Context) {
+	template := prompt.FromMessages(schema.GoTemplate,
+		schema.UserMessage(`Given i want to categorize transactions on my bank account into this categories: {{.categories}}
 In which category would a transaction from "{{.destination_name}}" with the subject "{{.description}}" fall into?
-Just output the name of the category. Does not have to be a complete sentence.`,
-		[]string{"categories", "destination_name", "description"},
+Just output the name of the category. Does not have to be a complete sentence.`),
 	)
-	_, _ = fmt.Println(prompt)
+	_, _ = fmt.Println(template)
 }
