86299954d540aa5f248bb6c9a67c6b03fde33384
sysatom <sysatom@gmail.com>
2026-05-05 16:45:39 +0800

    chore: remove unuseful modules

--- internal/modules/fx.go
+++ internal/modules/fx.go
@@ -1,20 +1,15 @@
 package modules
 
 import (
-	"github.com/flowline-io/flowbot/internal/modules/archive"
 	"github.com/flowline-io/flowbot/internal/modules/bookmark"
 	"github.com/flowline-io/flowbot/internal/modules/dev"
-	"github.com/flowline-io/flowbot/internal/modules/finance"
 	"github.com/flowline-io/flowbot/internal/modules/gitea"
 	"github.com/flowline-io/flowbot/internal/modules/github"
 	"github.com/flowline-io/flowbot/internal/modules/hub"
 	"github.com/flowline-io/flowbot/internal/modules/kanban"
 	"github.com/flowline-io/flowbot/internal/modules/notify"
 	"github.com/flowline-io/flowbot/internal/modules/reader"
-	"github.com/flowline-io/flowbot/internal/modules/search"
 	"github.com/flowline-io/flowbot/internal/modules/server"
-	"github.com/flowline-io/flowbot/internal/modules/torrent"
-	"github.com/flowline-io/flowbot/internal/modules/user"
 	"github.com/flowline-io/flowbot/internal/modules/webhook"
 	"github.com/flowline-io/flowbot/internal/modules/workflow"
 	"go.uber.org/fx"
@@ -23,20 +18,15 @@ import (
 // Modules registers all interaction modules.
 var Modules = fx.Options(
 	fx.Invoke(
-		archive.Register,
 		bookmark.Register,
 		dev.Register,
-		finance.Register,
 		gitea.Register,
 		github.Register,
 		hub.Register,
 		kanban.Register,
 		notify.Register,
 		reader.Register,
-		search.Register,
 		server.Register,
-		torrent.Register,
-		user.Register,
 		webhook.Register,
 		workflow.Register,
 	),
--- internal/modules/user/command.go
+++
@@ -1,7 +0,0 @@
-package user
-
-import (
-	"github.com/flowline-io/flowbot/pkg/types/ruleset/command"
-)
-
-var commandRules []command.Rule
