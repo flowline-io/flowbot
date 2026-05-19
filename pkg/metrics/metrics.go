4210c3259715e9e8832f9db5dd434b9b448a17a0
sysatom <sysatom@gmail.com>
2026-05-19 22:39:01 +0800

    feat(metrics): add Fx module

---
+++ pkg/metrics/metrics.go
@@ -0,0 +1,19 @@
+package metrics
+
+import (
+	"go.uber.org/fx"
+
+	"github.com/flowline-io/flowbot/pkg/stats"
+)
+
+func Module() fx.Option {
+	return fx.Module("metrics",
+		fx.Provide(
+			stats.NewStats,
+			NewPipelineCollector,
+			NewWorkflowCollector,
+			NewEventCollector,
+			NewAbilityCollector,
+		),
+	)
+}
