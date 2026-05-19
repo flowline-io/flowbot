efcf16da14f1044cd51ae5c507b1da98b9e9f639
sysatom <sysatom@gmail.com>
2026-05-19 15:54:13 +0800

    feat: apply conservative pool defaults in ent.NewClient test path

--- internal/store/ent/ent.go
+++ internal/store/ent/ent.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/jackc/pgx/v5/stdlib" //revive:disable:blank-imports pgx driver registration
 
 	gen "github.com/flowline-io/flowbot/internal/store/ent/gen"
+	"github.com/flowline-io/flowbot/internal/store/postgres"
 )
 
 // NewClient creates a new Ent client connected to a PostgreSQL database.
@@ -16,6 +17,7 @@ func NewClient(dsn string) (*gen.Client, error) {
 	if err != nil {
 		return nil, err
 	}
+	postgres.ApplyDefaults(db)
 	drv := entsql.OpenDB(dialect.Postgres, db)
 	return gen.NewClient(gen.Driver(drv)), nil
 }
