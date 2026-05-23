55aff3c6fa95dd1cb8c50b11c6944c89ee89ca9d
sysatom <sysatom@gmail.com>
2026-05-23 12:15:47 +0800

    feat(modules/example): add webhook route for example capability

--- internal/modules/example/webhook.go
+++ internal/modules/example/webhook.go
@@ -1,3 +1,24 @@
 package example
 
-var webhookRules []any
+import (
+	"github.com/gofiber/fiber/v3"
+
+	"github.com/flowline-io/flowbot/pkg/route"
+	"github.com/flowline-io/flowbot/pkg/types/ruleset/webservice"
+)
+
+var webhookRules = []webservice.Rule{
+	webservice.Post("/webhook/example", exampleWebhook, route.WithNotAuth()),
+}
+
+// exampleWebhook handles POST /service/example/webhook/example
+//
+//	@Summary	Receive example webhook events
+//	@Tags		example
+//	@Accept		json
+//	@Produce	json
+//	@Success	202	{string}	string	"Accepted"
+//	@Router		/example/webhook/example [post]
+func exampleWebhook(ctx fiber.Ctx) error {
+	return ctx.SendStatus(fiber.StatusAccepted)
+}
