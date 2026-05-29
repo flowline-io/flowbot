0dad31c090f6d2f34476e75f7ea1853cdcfe4dd8
sysatom <sysatom@gmail.com>
2026-05-29 15:58:34 +0800

    refactor: remove server, notify, workflow modules and their fx registration

--- internal/modules/fx.go
+++ internal/modules/fx.go
@@ -6,9 +6,6 @@ import (
 
 	"github.com/flowline-io/flowbot/internal/modules/example"
 	"github.com/flowline-io/flowbot/internal/modules/hub"
-	"github.com/flowline-io/flowbot/internal/modules/notify"
-	"github.com/flowline-io/flowbot/internal/modules/server"
-	"github.com/flowline-io/flowbot/internal/modules/workflow"
 )
 
 // Modules registers all interaction modules.
@@ -16,8 +13,5 @@ var Modules = fx.Options(
 	fx.Invoke(
 		example.Register,
 		hub.Register,
-		notify.Register,
-		server.Register,
-		workflow.Register,
 	),
 )
