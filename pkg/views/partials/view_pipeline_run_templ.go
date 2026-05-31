c9d6178775c16374232a727cfa2118d6de974057
sysatom <sysatom@gmail.com>
2026-05-31 19:36:06 +0800

    fix: export template function names, regenerate templ code

--- pkg/views/partials/view_pipeline_run_templ.go
+++ pkg/views/partials/view_pipeline_run_templ.go
@@ -12,7 +12,7 @@ import (
 	gen "github.com/flowline-io/flowbot/internal/store/ent/gen"
 )
 
-func viewPipelineRunContent(steps any) templ.Component {
+func ViewPipelineRunContent(steps any) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
