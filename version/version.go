a0e53f680f7c9140272f34d7dcff67cb409e998c
sysatom <sysatom@gmail.com>
2026-07-19 00:36:51 +0800

    chore: update version to v0.97.0
    - Incremented the Buildtags variable in version.go to reflect the new version number, ensuring accurate versioning for the application.

--- version/version.go
+++ version/version.go
@@ -12,4 +12,4 @@ var Buildstamp = "undef"
 // Buildtags set it to git tag:
 //
 //	-ldflags "-X github.com/flowline-io/flowbot/version.Buildtags=`git describe --tags`"
-var Buildtags = "v0.96.2"
+var Buildtags = "v0.97.0"
